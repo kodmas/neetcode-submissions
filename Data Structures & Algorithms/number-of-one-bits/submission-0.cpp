6aafcb77015ba8958664d309061a886346da2b45
JJJJJerry <74963031+kodmas@users.noreply.github.com>
2026-07-01 00:06:30 +0800

    Add: number-of-one-bits - submission-0

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while(n >= 1){
+            // cout << (n&1) << endl;
+            ans += n&1;
+            n >>= 1;
+        }
+
+        return ans;
+    }
+};
